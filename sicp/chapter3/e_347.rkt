b8873ddd0c0c8488a1ea451627252c42b34a7b4f
AliceLanniste <1399789151@qq.com>
2019-08-12 22:21:42 +0800

    complete 3.4

--- sicp/chapter3/e_347.rkt
+++ sicp/chapter3/e_347.rkt
@@ -1,25 +1,3 @@
-#lang racket
-
-;;多个mutex使用
-
-(define (make-mutex)
-  (let ((cell (list false)))
-    (define (the-mutex m)
-      (cond ((eq? m 'acquire)
-             (if (test-and-set! cell)
-                 (the-mutex 'acquire))) ; retry
-            ((eq? m 'release) (clear! cell))))
-    the-mutex))
-(define (clear! cell) (set-car! cell false))
-
-(define (test-and-set! cell)
-  (if (car cell)
-      true
-      (begin (set-car! cell true)
-             false)))
-
-
-
 (define (make-semaphore n) 
    (let ((lock (make-mutex)) 
          (taken 0)) 
@@ -27,10 +5,28 @@
        (cond ((eq? command 'acquire) 
               (lock 'acquire) 
               (if (< taken n) 
-                  (begin (set! taken (+ 1 taken)) (lock 'release)) 
+                  (begin (set! taken (+  taken 1)) (lock 'release)) 
                   (begin (lock 'release) (semaphore 'acquire)))) 
              ((eq? command 'release) 
               (lock 'acquire) 
               (set! taken (- taken 1)) 
               (lock 'release)))) 
-     semaphore)) 
+     semaphore))
+
+
+(define (make-semaphore2 n)
+  (let ((taken 0)
+        (cell (list false)))
+     (define (semaphore c)
+       (cond ((eq? c 'accquire)
+               (if (test-and-set! cell)
+                    (semaphore 'accquire))
+               (if (< taken n)
+                   (begin (set! taken (+ taken 1)) (clear! cell))
+                    (begin (clear! cell) (semaphore 'accquire))))
+             ((eq? c 'release)
+               (if (test-and-set! cell)
+                    (semaphore 'release))
+               (set! taken (- taken 1))
+               (clear! cell))))
+    semaphore))
